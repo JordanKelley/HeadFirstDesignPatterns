4288aa2b601c00c4e2bfc7305b17dee586ebc7bb
Jordan Kelley <jordan_kelley@choosebroadspire.com>
2019-11-06 17:44:00 -0600

    missing file

---
+++ HeadFirstDesignPatterns/CommandPattern/HottubOffCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirstDesignPatterns.CommandPattern
+{
+    public class HottubOffCommand : Command
+    {
+        private Hottub hottub;
+
+        public HottubOffCommand(Hottub hottub)
+        {
+            this.hottub = hottub;
+        }
+
+        public void execute()
+        {
+            hottub.off();
+        }
+
+        public void undo()
+        {
+            hottub.on();
+        }
+    }
+}
